b559c563787fc1d015e60e88fdbeb9cc99afdce6
Jaeseok Park <jspark@aixcon.co.kr>
2022-01-19 00:50:37 +0900

    게시판 목록 페이지

--- board/views.py
+++ board/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    # 쿼리 날려서 온 결과(역순으로)
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'board/index.html',
+        {
+            # 쿼리 날려서 온 결과를 딕셔너리 형태로 저장
+            'posts': posts,
+        }
+    )
