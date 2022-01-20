d4fb14c93cd456e81c8de0d639dcd7efab654431
Jaeseok Park <m2363@naver.com>
2022-01-20 17:45:47 +0900

    이미지 업로드 기능 추가

--- board/models.py
+++ board/models.py
@@ -4,14 +4,21 @@ from django.db import models
 class Post(models.Model):
     # 게시글 제목
     title = models.CharField(max_length=50)
+
     # 글 내용
     content = models.TextField()
+
     # 작성일(작성된 날짜 자동 등록)
     created_at = models.DateTimeField(auto_now_add=True)
+
     # 수정 시 수정된 날짜 새로 생성
     updated_at = models.DateTimeField(auto_now=True)
+
+    # 이미지 업로드
+    image = models.ImageField(upload_to='board/images/%Y/%m/%d/', blank=True)
     # 작성자
     # author
+
     # 카테고리
     # category
 
