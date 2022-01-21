a18def8eff6fb5c5c87809500535c5269b860ce3
Jaeseok Park <m2363@naver.com>
2022-01-21 11:34:10 +0900

    이미지 필드 삭제 후 파일 업로드랑 통합, 상세보기 페이지에 파일 업로드 다운로드 구현

--- board/models.py
+++ board/models.py
@@ -14,11 +14,9 @@ class Post(models.Model):
     # 수정 시 수정된 날짜 새로 생성
     updated_at = models.DateTimeField(auto_now=True)
 
-    # 이미지 업로드
-    image_upload = models.ImageField(upload_to='board/images/%Y/%m/%d/', blank=True)
-
     # 파일 업로드
     file_upload = models.FileField(upload_to='board/files/%Y/%m/%d/', blank=True)
+
     # 작성자
     # author
 
@@ -31,9 +29,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/board/{self.pk}/'
-
-    def get_image_name(self):
-        return os.path.basename(self.image_upload.name)
 
     def get_file_name(self):
         return os.path.basename(self.file_upload.name)
