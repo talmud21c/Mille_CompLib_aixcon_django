7c617fe8ff16135119797a931988749a82511d59
Jaeseok Park <m2363@naver.com>
2022-02-10 13:06:32 +0900

    uwsgi, nginx settings

--- django_board_prj/settings.py
+++ django_board_prj/settings.py
@@ -128,6 +128,7 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "/board/static/board/image")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
